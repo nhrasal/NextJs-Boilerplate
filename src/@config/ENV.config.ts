b0340821a0e4afdb7f6606a4cadf66ff419e25f2
nhrasal <nhrasal.cse@gmail.com>
2022-12-27 12:06:33 +0600

    env and env config refactoring

--- src/@config/ENV.config.ts
+++ src/@config/ENV.config.ts
@@ -1,19 +1,19 @@
 interface IENV {
-    env: string;
-    ApiEndpoint: string;
-    AdminURL?: string;
-    HostName?: string;
-    LandingPageURL?: string;
-    IMAGE_BASE_URL?: string;
-    GOOGLE_RECAPTCHA?: string;
+  env: string | null;
+  ApiEndpoint: string | null;
+  AdminURL?: string | null;
+  HostName?: string | null;
+  LandingPageURL?: string | null;
+  IMAGE_BASE_URL?: string | null;
+  GOOGLE_RECAPTCHA?: string | null;
 }
 
 export const ENV: IENV = {
-    env: process.env.NEXT_PUBLIC_ENV || "",
-    ApiEndpoint: process.env.NEXT_PUBLIC_BASE_URL || "",
-    HostName: process.env.NEXT_PUBLIC_HOST_NAME || "",
-    AdminURL: process.env.NEXT_PUBLIC_ADMIN_URL || "",
-    LandingPageURL: process.env.NEXT_PUBLIC_LANDING_PAGE || "",
-    IMAGE_BASE_URL: process.env.NEXT_PUBLIC_IMAGE_BASE_URL || "",
-    GOOGLE_RECAPTCHA: process.env.NEXT_PUBLIC_GOOGLE_RECAPTCHA || ""
+  env: process.env.NEXT_PUBLIC_ENV || null,
+  ApiEndpoint: process.env.NEXT_PUBLIC_BASE_URL || null,
+  HostName: process.env.NEXT_PUBLIC_HOST_NAME || null,
+  AdminURL: process.env.NEXT_PUBLIC_ADMIN_URL || null,
+  LandingPageURL: process.env.NEXT_PUBLIC_LANDING_PAGE || null,
+  IMAGE_BASE_URL: process.env.NEXT_PUBLIC_IMAGE_BASE_URL || null,
+  GOOGLE_RECAPTCHA: process.env.NEXT_PUBLIC_GOOGLE_RECAPTCHA || null,
 };
